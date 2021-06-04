4ee73666f3388f257c2ed94ce1212d41ead4adb3
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-04 14:50:51 -0300

    uso do filter em objetos e exemplo com .includes

--- script.js
+++ script.js
@@ -45,3 +45,38 @@ const randomNumbers = [36, 99, 37, 63];
 const numbersGreaterThan37 = randomNumbers.filter(item => item > 37);
 
 console.log(numbersGreaterThan37);
+
+//Exemplo com objeto usando FILTER
+const tarantinoMovies = [
+    {name: 'Bartardos Inglórios', release: 2009},
+    {name: 'Pulp Fiction', release: 1994},
+    {name: 'Kill Bill: Volume 2', release: 2004},
+    {name: 'Quatro Quartos', release: 1995},
+    {name: 'Sin City', release: 2005},
+    {name: 'Era uma vez em... Hollywood', release: 2019},
+    {name: 'Django Livre', release: 2012},
+    {name: 'Cães de Aluguel', release: 1992},
+    {name: 'À Prova da Morte', release: 2007},
+    {name: 'Kill Bill: Volume 1', release: 2003},
+]
+
+const moviesBefore2000 = tarantinoMovies.filter(({ release }) => release < 2000);
+
+console.log(moviesBefore2000);
+console.table(moviesBefore2000);
+
+// outro exemplo 
+
+const firstTravelerCities = [
+    'Sydney', 'Berlim', 'Lisboa', 'Sófia', 'Praga', 'Bali', 'Florianópolis'
+]
+
+const secondTravelerCities = [
+    'Praga', 'Roma', 'Chiang Mai', 'Lisboa', 'Zagreb'
+]
+
+// exibir as cidades em comum
+
+const commomCities = firstTravelerCities.filter(city => secondTravelerCities.includes(city));
+
+console.log(commomCities)
