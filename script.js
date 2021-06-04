4803d031fd757a2331e15aaa21c088f965a56434
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-04 15:20:55 -0300

    exemplo de reduce retornando objeto

--- script.js
+++ script.js
@@ -112,3 +112,25 @@ esperado nesse formato:
     - Nome 3
 */
 
+const people = [
+    { id: 5, name: 'Angelica', age: 18, federativeUnit: 'Pernambuco'},
+    { id: 81, name: 'Thales', age: 19, federativeUnit: 'São Paulo'},
+    { id: 47, name: 'Ana Carolina', age: 18, federativeUnit: 'Alagoas'},
+    { id: 87, name: 'Felipe', age: 18, federativeUnit: 'Minas Gerais'},
+    { id: 9, name: 'Gabriel', age: 20, federativeUnit: 'São Paulo'},
+    { id: 73, name: 'Aline', age: 19, federativeUnit: 'Brasília'},
+]
+
+const agesFrequency = people.reduce((accumulator, { age }) => {
+    accumulator[age] = accumulator[age] + 1 || 1 ;
+
+    return accumulator
+}, {});
+
+console.log(agesFrequency);
+
+/*
+resultdo desejado:
+
+    { 18: 3, 19: 2, 20: 1 }
+*/
