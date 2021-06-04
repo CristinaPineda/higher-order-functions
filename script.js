2a55221b7ea82a528b3ec619e27bd1115fd40978
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-04 15:07:47 -0300

    uso do reduce e uso do reduce para retornar uma string

--- script.js
+++ script.js
@@ -40,43 +40,75 @@
 // um novo array com somete alguns itens 
 // do array original
 
-const randomNumbers = [36, 99, 37, 63];
+// const randomNumbers = [36, 99, 37, 63];
 
-const numbersGreaterThan37 = randomNumbers.filter(item => item > 37);
+// const numbersGreaterThan37 = randomNumbers.filter(item => item > 37);
 
-console.log(numbersGreaterThan37);
+// console.log(numbersGreaterThan37);
 
-//Exemplo com objeto usando FILTER
-const tarantinoMovies = [
-    {name: 'Bartardos Inglórios', release: 2009},
-    {name: 'Pulp Fiction', release: 1994},
-    {name: 'Kill Bill: Volume 2', release: 2004},
-    {name: 'Quatro Quartos', release: 1995},
-    {name: 'Sin City', release: 2005},
-    {name: 'Era uma vez em... Hollywood', release: 2019},
-    {name: 'Django Livre', release: 2012},
-    {name: 'Cães de Aluguel', release: 1992},
-    {name: 'À Prova da Morte', release: 2007},
-    {name: 'Kill Bill: Volume 1', release: 2003},
+// //Exemplo com objeto usando FILTER
+// const tarantinoMovies = [
+//     {name: 'Bartardos Inglórios', release: 2009},
+//     {name: 'Pulp Fiction', release: 1994},
+//     {name: 'Kill Bill: Volume 2', release: 2004},
+//     {name: 'Quatro Quartos', release: 1995},
+//     {name: 'Sin City', release: 2005},
+//     {name: 'Era uma vez em... Hollywood', release: 2019},
+//     {name: 'Django Livre', release: 2012},
+//     {name: 'Cães de Aluguel', release: 1992},
+//     {name: 'À Prova da Morte', release: 2007},
+//     {name: 'Kill Bill: Volume 1', release: 2003},
+// ]
+
+// const moviesBefore2000 = tarantinoMovies.filter(({ release }) => release < 2000);
+
+// console.log(moviesBefore2000);
+// console.table(moviesBefore2000);
+
+// // outro exemplo 
+
+// const firstTravelerCities = [
+//     'Sydney', 'Berlim', 'Lisboa', 'Sófia', 'Praga', 'Bali', 'Florianópolis'
+// ]
+
+// const secondTravelerCities = [
+//     'Praga', 'Roma', 'Chiang Mai', 'Lisboa', 'Zagreb'
+// ]
+
+// // exibir as cidades em comum
+
+// const commomCities = firstTravelerCities.filter(city => secondTravelerCities.includes(city));
+
+// console.log(commomCities);
+
+// -------------------------------------------------------------------
+
+// REDUCE
+
+// Quando usar o reduce?
+// Você quer reduzir o array à algum tipo de dado
+
+const numbers = [1, 2, 3]
+const sum = numbers.reduce((accumulator, item) => accumulator + item, 0);
+
+console.log(sum);
+
+const cart = [
+    { name: 'Dark Souls III', price: 95.03 },
+    { name: 'Shadow of the Tomb Raider', price: 101.19 },
+    { name: 'Shekiro: Shadows Die Twice', price: 179.99 },
+    { name: 'Resident Evil 2', price: 119.90 },
+    { name: 'Death Stranding', price: 149.99 },
 ]
 
-const moviesBefore2000 = tarantinoMovies.filter(({ release }) => release < 2000);
+const productList = cart.reduce((accumulator, { name }) => `${accumulator}- ${name}\n`, '');
 
-console.log(moviesBefore2000);
-console.table(moviesBefore2000);
+console.log(productList)
+/*
+esperado nesse formato: 
 
-// outro exemplo 
+    - Nome 1
+    - Nome 2
+    - Nome 3
+*/
 
-const firstTravelerCities = [
-    'Sydney', 'Berlim', 'Lisboa', 'Sófia', 'Praga', 'Bali', 'Florianópolis'
-]
-
-const secondTravelerCities = [
-    'Praga', 'Roma', 'Chiang Mai', 'Lisboa', 'Zagreb'
-]
-
-// exibir as cidades em comum
-
-const commomCities = firstTravelerCities.filter(city => secondTravelerCities.includes(city));
-
-console.log(commomCities)
