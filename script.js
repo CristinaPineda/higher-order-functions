4ce1cfdd1d6f8acda603bf4fa0a9a0cd2bbbc8d4
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-06-04 14:10:03 -0300

    metodo MAP

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// function getMultiplier (multiplier) {
-//     return function (aNumber) {
-//         return aNumber * multiplier
-//     }
-// }
+// MAP
 
-// refatorando a função acima 
-const getMultiplier = multiplier => aNumber => aNumber * multiplier
+// Quando usar o map?
+// Você quiser obter um novo array com a mesma quantidade
+// de itens do original mas, com cada item transformado. 
+// ele vai percorrer cada item e retornar um novo array com a transformação que vc quer
+// pode receber 3 parametros: 
+// .map((item, index, array))
 
-const double = getMultiplier(2);
-const triple = getMultiplier(3);
-const quadruple = getMultiplier(4);
+const numbers = [1, 2, 3]
 
-console.log(double(5))
-console.log(triple(5))
-console.log(quadruple(5))
+const squareNumbers = numbers.map(item => item ** 2 );
+console.log (squareNumbers);
+
